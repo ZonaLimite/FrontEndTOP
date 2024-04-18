15cb112f21eaa9824de5dd5a4f7759411d4f1aa8
Paco Boga <javier.boga.rioja@gmail.com>
2024-04-19 00:24:06 +0200

    Esto era de la mañana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooteroComponent } from './components/footero/footero.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
@@ -12,7 +11,6 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooteroComponent,
     FooterComponent,
     SidebarComponent
   ],
