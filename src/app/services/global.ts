8837566a42ffc39a270439a406beea24ca81d61f
Paco Boga <javier.boga.rioja@gmail.com>
2024-05-16 22:23:15 +0200

    Version final. Servidores en 192.168.1.5

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,9 +1,4 @@
 export var GLOBAL = {
-<<<<<<< HEAD
-    urlBase: 'http://21.4.1.7:8080/',
-    urlBaseEngine: 'http://21.4.1.7:8090/',
-=======
-    urlBase: 'http://localhost:8080/',
-    urlBaseEngine: 'http://localhost:8090/',
->>>>>>> aef79fe3f3c7659f1bc31becfa3408d7c375e94d
+    urlBase: 'http://192.168.1.5:8080/',
+    urlBaseEngine: 'http://192.168.1.5:8090/',
 }
